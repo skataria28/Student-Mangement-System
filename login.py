fb28991dbdaa250bf661eac2b2b0f8f31e825892
skataria28 <136513872+skataria28@users.noreply.github.com>
2024-03-25 03:41:21 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,67 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import ImageTk
+
+def login():
+    if usernameEntry.get()==''or passwordEntry.get()=='':
+        messagebox.showerror('Error','Feilds cannot be empty')
+    elif usernameEntry.get()=='sheetal' and passwordEntry.get()=='1234':
+        messagebox.showinfo('success','Welcome')
+
+        window.destroy()
+        import sms
+
+
+    else:
+        messagebox.showerror('Error','Please enter correct credentials')
+
+
+# Design the login system in this project
+window = Tk()
+window.geometry('1280x700+0+0')
+window.title('Login System Of Student Management System')
+
+window.resizable(False, False)
+
+backgroundImage=ImageTk.PhotoImage(file='bg.jpg')
+
+bgLabel=Label(window,image=backgroundImage)
+bgLabel.place(x=0,y=0)
+
+loginFrame=Frame(window,bg='white')
+loginFrame.place(x=400,y=150)
+
+
+logoImage = PhotoImage(file='logo.png')
+
+logoLabel=Label(loginFrame, image=logoImage)
+logoLabel.grid(row=0,column=0,columnspan=2,pady=10)
+
+usernameImage=PhotoImage(file='user.png')
+usernameLabel=Label(loginFrame,image=usernameImage,text='Username',compound=LEFT,font=('times new roman',20,'bold'),bg='white')
+usernameLabel.grid(row=1,column=0,pady=10,padx=20)
+
+usernameEntry=Entry(loginFrame,font=('times new roman',20,'bold'),bd=5,fg='royalblue')
+usernameEntry.grid(row=1,column=1,pady=10,padx=20)
+
+
+# for password..
+
+passwordImage=PhotoImage(file='password.png')
+usernameLabel=Label(loginFrame,image=passwordImage,text='Password',compound=LEFT,font=('times new roman',20,'bold'),
+                    bg='white')
+usernameLabel.grid(row=2,column=0,pady=10,padx=20)
+
+passwordEntry=Entry(loginFrame,font=('times new roman',20,'bold'),bd=5,fg='royalblue')
+passwordEntry.grid(row=2,column=1,pady=10,padx=20)
+
+loginButton=Button(loginFrame,text='Login',width=15,font=('times new roman',14,'bold'),fg='white',
+                   bg='cornflowerblue',cursor='hand2',command=login)
+loginButton.grid(row=3,column=1,pady=10)
+
+
+
+
+
+
+window.mainloop()
---
+++ sms.py
@@ -0,0 +1,256 @@
+from tkinter import *
+import time
+from tkinter import ttk,messagebox
+import pymysql
+
+
+
+#functionallity part
+
+def add_student():
+    def add_data():
+        if idEntry.get()=='' or nameEntry.get()=='' or phoneEntry.get()=='' or emailEntry.get()=='' or addressEntry.get()=='' or genderEntry.get()=='' or dobEntry.get()=='':
+            messagebox.showerror('Error','All Feilds are required',parent=add_window)
+
+
+        else:
+            currentdate = time.strftime('%d/%m/%Y')
+            currenttime = time.strftime('%H:%M:%S')
+            try:
+                query='insert into student values(%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+                mycursor.execute(query,(idEntry.get(),nameEntry.get(),phoneEntry.get(),emailEntry.get(),addressEntry.get(),
+                                        genderEntry.get(),dobEntry.get(),currentdate,currenttime))
+                con.commit()
+                result=messagebox.askyesno('Confirm','Data added successfully. Do you want to clean the form?',parent=add_window)
+                if result:
+                    idEntry.delete(0,END)
+                    nameEntry.delete(0,END)
+                    phoneEntry.delete(0,END)
+                    emailEntry.delete(0,END)
+                    addressEntry.delete(0,END)
+                    genderEntry.delete(0,END)
+                    dobEntry.delete(0,END)
+                else:
+                    pass
+            except:
+                messagebox.showerror('Error','Id cannot be repeated',parent=add_window)
+                return
+
+
+
+            query= 'select * from student'
+            mycursor.execute(query)
+            fetched_data=mycursor.fetchall()
+            studentTable.delete(*studentTable.get_children())
+            for  data in fetched_data:
+                datalist=list(data)
+                studentTable.insert('',END,values=datalist)
+
+
+    add_window=Toplevel()
+    add_window.grab_set()
+    add_window.resizable(False,False)
+    idLabel=Label(add_window,text='Id',font=('times new roman',20,'bold'))
+    idLabel.grid(row=0,column=0,padx=30,pady=15,sticky=W)
+    idEntry=Entry(add_window,font=('roman',15,'bold'),width=24)
+    idEntry.grid(row=0,column=1,pady=15,padx=10)
+
+    nameLabel = Label(add_window, text='Name', font=('times new roman', 20, 'bold'))
+    nameLabel.grid(row=1, column=0, padx=30, pady=15,sticky=W)
+    nameEntry = Entry(add_window, font=('roman', 15, 'bold'), width=24)
+    nameEntry.grid(row=1, column=1, pady=15, padx=10)
+
+    phoneLabel = Label(add_window, text='Phone', font=('times new roman', 20, 'bold'))
+    phoneLabel.grid(row=2, column=0, padx=30, pady=15,sticky=W)
+    phoneEntry = Entry(add_window, font=('roman', 15, 'bold'), width=24)
+    phoneEntry.grid(row=2, column=1, pady=15, padx=10)
+
+    emailLabel = Label(add_window, text='Email', font=('times new roman', 20, 'bold'))
+    emailLabel.grid(row=3, column=0, padx=30, pady=15,sticky=W)
+    emailEntry = Entry(add_window, font=('roman', 15, 'bold'), width=24)
+    emailEntry.grid(row=3, column=1, pady=15, padx=10)
+
+    addressLabel = Label(add_window, text='Address', font=('times new roman', 20, 'bold'))
+    addressLabel.grid(row=4, column=0, padx=30, pady=15,sticky=W)
+    addressEntry = Entry(add_window, font=('roman', 15, 'bold'), width=24)
+    addressEntry.grid(row=4, column=1, pady=15, padx=10)
+
+    genderLabel = Label(add_window, text='Gender', font=('times new roman', 20, 'bold'))
+    genderLabel.grid(row=5, column=0, padx=30, pady=15,sticky=W)
+    genderEntry = Entry(add_window, font=('roman', 15, 'bold'), width=24)
+    genderEntry.grid(row=5, column=1, pady=15, padx=10)
+
+    dobLabel = Label(add_window, text='D.O.B', font=('times new roman', 20, 'bold'))
+    dobLabel.grid(row=6, column=0, padx=30, pady=15,sticky=W)
+    dobEntry = Entry(add_window, font=('roman', 15, 'bold'), width=24)
+    dobEntry.grid(row=6, column=1, pady=15, padx=10)
+
+    add_student_button=ttk.Button(add_window,text='ADD STUDENT',command=add_data)
+    add_student_button.grid(row=7,columnspan=2,pady=15)
+
+
+#connect to database dialog box/page
+def connect_database():
+    def connect():
+        global mycursor,con
+        try:
+            con = pymysql.connect(host=hostentry.get(), user=usernameentry.get(), password=passwordentry.get())
+            mycursor = con.cursor()
+        except:
+            messagebox.showerror('Error','Invalid Details',parent=connectWindow)
+            return
+        try:
+            query='create database studentmanagementsystem'
+            mycursor.execute(query)
+            query='use studentmanagementsystem'
+            mycursor.execute(query)
+            query='create table student(id int not null primary key, name varchar(30),mobile  varchar(10),email varchar(30),' \
+                  'address varchar(50),gender varchar(20),dob varchar(20),date varchar(50),time varchar(50))'
+            mycursor.execute(query)
+        except:
+            query='use studentmanagementsystem'
+            mycursor.execute(query)
+        messagebox.showinfo('Success', 'Database Connection is sucessful', parent=connectWindow)
+        connectWindow.destroy()
+
+        addstudentButton.config(state=NORMAL)
+        searchstudentButton.config(state=NORMAL)
+        updatestudentButton.config(state=NORMAL)
+        showstudentButton.config(state=NORMAL)
+        exportstudentButton.config(state=NORMAL)
+        deletestudentButton.config(state=NORMAL)
+
+    connectWindow=Toplevel()
+    connectWindow.grab_set()
+    connectWindow.geometry('470x250+770+230')
+    connectWindow.title('Database Connection')
+    connectWindow.resizable(0,0)
+
+    hostnamelabel=Label(connectWindow,text='Host Name',font=('arial',20,'bold'))
+    hostnamelabel.grid(row=0,column=0,padx=20)
+
+    hostentry=Entry(connectWindow,font=('roman',15,'bold'),bd=2)
+    hostentry.grid(row=0,column=1,padx=40,pady=20)
+
+    usernamelabel = Label(connectWindow, text='User Name', font=('arial', 20, 'bold'))
+    usernamelabel.grid(row=1, column=0, padx=20)
+
+    usernameentry = Entry(connectWindow, font=('roman', 15, 'bold'), bd=2)
+    usernameentry.grid(row=1, column=1, padx=40, pady=20)
+
+    passwordlabel = Label(connectWindow, text='Password', font=('arial', 20, 'bold'))
+    passwordlabel.grid(row=2, column=0, padx=20)
+
+    passwordentry = Entry(connectWindow, font=('roman', 15, 'bold'), bd=2)
+    passwordentry.grid(row=2, column=1, padx=40, pady=20)
+
+    connectButton=Button(connectWindow,text='CONNECT',command=connect)
+    connectButton.grid(row=3,columnspan=2)
+
+count=0
+text=''
+def slider():
+    global text,count
+    if count==len(s):
+        count=0
+        text=''
+    text=text+s[count]
+    sliderlabel.config(text=text)
+    count+=1
+    sliderlabel.after(300,slider)
+
+
+
+def clock():
+    date= time.strftime('%d/%m/%Y')
+    currenttime=time.strftime('%H:%M:%S')
+    datetimeLabel.config(text=f'   Date:{date}\nTime:{currenttime}')
+    datetimeLabel.after(1000,clock)
+
+#gui part
+root= Tk()
+
+root.wm_geometry('1174x680+0+0')
+root.wm_resizable(0,0)
+root.title('Student Management System')
+
+datetimeLabel=Label(root,text='hello',font=('times new roman',18,'bold'))
+datetimeLabel.place(x=5,y=5)
+clock()
+
+s='Student Management System'
+sliderlabel=Label(root,font=('arial',28,'italic bold'),width=30)
+sliderlabel.place(x=200,y=0)
+slider()
+
+#here i added connect database command
+connectButton=Button(root,text='Connect Database',command=connect_database)
+connectButton.place(x=980,y=0)
+
+leftFrame=Frame(root)
+leftFrame.place(x=50,y=80,width=300,height=600)
+
+logo_image=PhotoImage(file='student (1).png')
+logo_label=Label(leftFrame,image=logo_image)
+logo_label.grid(row=0,column=0)
+
+addstudentButton=Button(leftFrame,text='Add Student',width=25,state=DISABLED,command=add_student)
+addstudentButton.grid(row=1,column=0,pady=20)
+
+searchstudentButton=Button(leftFrame,text='Search Student',width=25,state=DISABLED)
+searchstudentButton.grid(row=2,column=0,pady=20)
+
+deletestudentButton=Button(leftFrame,text='Delete Student',width=25,state=DISABLED)
+deletestudentButton.grid(row=3,column=0,pady=20)
+
+updatestudentButton=Button(leftFrame,text='Update Student',width=25,state=DISABLED)
+updatestudentButton.grid(row=4,column=0,pady=20)
+
+showstudentButton=Button(leftFrame,text='Show Student',width=25,state=DISABLED)
+showstudentButton.grid(row=5,column=0,pady=20)
+
+exportstudentButton=Button(leftFrame,text='Export Student',width=25,state=DISABLED)
+exportstudentButton.grid(row=6,column=0,pady=20)
+
+exitButton=Button(leftFrame,text='Exit',width=25)
+exitButton.grid(row=7,column=0,pady=20)
+
+#Right side frame
+
+rightFrame=Frame(root)
+rightFrame.place(x=350,y=80,width=820,height=600)
+
+scrollBarX= Scrollbar(rightFrame,orient=HORIZONTAL)
+scrollBarY= Scrollbar(rightFrame,orient=VERTICAL )
+
+#created tree view... :)
+studentTable=ttk.Treeview(rightFrame,columns=('Id','Name','Mobile No','Email','Address','Gender',
+                                 'D.O.B','Added Date','Added Time'),
+                          xscrollcommand=scrollBarX.set,yscrollcommand=scrollBarY.set)
+
+scrollBarX.config(command=studentTable.xview)
+scrollBarY.config(command=studentTable.yview)
+
+scrollBarX.pack(side=BOTTOM,fill=X)
+scrollBarY.pack(side=RIGHT,fill=Y)
+
+studentTable.pack(fill=BOTH,expand=1)
+
+studentTable.heading('Id',text='Id')
+studentTable.heading('Name',text='Name')
+studentTable.heading('Mobile No',text='Mobile No')
+studentTable.heading('Email',text='Email Address')
+studentTable.heading('Address',text='Address')
+studentTable.heading('Gender',text='Gender')
+studentTable.heading('D.O.B',text='D.O.B')
+studentTable.heading('Added Date',text='Added Date')
+studentTable.heading('Added Time',text='Added Time')
+
+
+
+
+
+studentTable.config(show='headings')
+
+
+root.mainloop()
